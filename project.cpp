e4c17a7c0d1fa1453ec304eab78f697bacbd0c65
Mrunal Amin <Mamin1581@conestogac.on.ca>
2024-08-11 15:50:15 -0400

    created a another struct and created a function createNode

--- project.cpp
+++ project.cpp
@@ -11,3 +11,29 @@ typedef struct Parcel {
     int weight;
     float value;
 } Parcel;
+
+typedef struct Node {
+    Parcel parcel;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+Node* hashTable[HASH_TABLE_SIZE];
+
+Node* createNode(Parcel parcel) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode) {
+        newNode->parcel.destination = (char*)malloc(strlen(parcel.destination) + 1);
+        if (newNode->parcel.destination) {
+            strcpy(newNode->parcel.destination, parcel.destination);
+        }
+        else {
+            free(newNode);
+            return NULL;
+        }
+        newNode->parcel.weight = parcel.weight;
+        newNode->parcel.value = parcel.value;
+        newNode->left = newNode->right = NULL;
+    }
+    return newNode;
+}
